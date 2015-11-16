9caa471a4161ae4a90b066d6ee1fe8adf32c4e50
Hays Hutton <hhutton@spurpose.com>
2015-11-16 13:28:33 -0600

    debug

--- index.js
+++ index.js
@@ -19,17 +19,25 @@ process.stdin.on('readable', function() {
 process.stdin.on('end', function() {
   var bead = JSON.parse(flowFile);
   
+  console.log("orig===============");
+  console.log(JSON.stringify(flowFile,null,2);
+
   if(bead.latitude !== undefined && bead.longitude !== undefined) {
-    request('https://api.forecast.io/forecast/' +
+    var url = 'https://api.forecast.io/forecast/' +
             forecastEnv.apiKey +
             '/' +
-            bead.latitude + ',' + bead.longitude + ',' + bead.localISO,
+            bead.latitude + ',' + bead.longitude + ',' + bead.localISO 
+    request(url,
             function(err, resp, body) {
               if(err) {
                 throw err;
               };
               var forecast = JSON.parse(body);
+              console.log("forecast===============");
+              console.log(JSON.stringify(body,null,2);
               var newBead = _.assign(forecast.currently, bead);
+              console.log("newBead===============");
+              console.log(JSON.stringify(newBead,null,2);
               process.stdout.write(JSON.stringify(newBead, null, 2));
               process.exit(0);
               });
