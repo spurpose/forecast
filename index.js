262493afd8e61479e6ebea252f2affb05616380a
Hays Hutton <hhutton@spurpose.com>
2015-11-16 14:02:56 -0600

    adding time to api call

--- index.js
+++ index.js
@@ -19,28 +19,22 @@ process.stdin.on('readable', function() {
 process.stdin.on('end', function() {
   var bead = JSON.parse(flowFile);
   
-  console.log("orig===============");
-  console.log(JSON.stringify(flowFile,null,2));
-
-  if(bead.latitude !== undefined && bead.longitude !== undefined) {
+  if(bead.latitude !== undefined && bead.longitude !== undefined && bead.localISO !== undefined) {
+    var time = bead.localISO.split('.')[0];
     var url = 'https://api.forecast.io/forecast/' +
-            forecastEnv.apiKey +
-            '/' +
-            bead.latitude + ',' + bead.longitude + ',' + bead.localISO 
-    request(url,
-            function(err, resp, body) {
-              if(err) {
-                throw err;
-              };
-              var forecast = JSON.parse(body);
-              console.log("forecast===============");
-              console.log(JSON.stringify(body,null,2));
-              var newBead = _.assign(forecast.currently, bead);
-              console.log("newBead===============");
-              console.log(JSON.stringify(newBead,null,2));
-              process.stdout.write(JSON.stringify(newBead, null, 2));
-              process.exit(0);
-              });
+            forecastEnv.apiKey + '/' +
+            bead.latitude + ',' +
+            bead.longitude + ',' +
+            time;
+    request(url, function(err, resp, body) {
+      if(err) {
+        throw err;
+      };
+      var forecast = JSON.parse(body);
+      var newBead = _.assign(forecast.currently, bead);
+      process.stdout.write(JSON.stringify(newBead, null, 2));
+      process.exit(0);
+      });
   } else {
     process.stdout.write(JSON.stringify(bead, null, 2));
     process.exit(0);
